67a4325bef23a478d7f7e59654bb7e6c3f6af3be
曹建兵 <caojb@hlzq.com>
2019-06-10 10:36:16 +0800

    Mod:修改了Hello和World

--- src/main/java/person/bingcod/study/World.java
+++ src/main/java/person/bingcod/study/World.java
@@ -2,6 +2,6 @@ package person.bingcod.study;
 
 public class World {
     public static void main(String[] args) {
-        System.out.println("World.");
+        System.out.println("您好，World.");
     }
 }
